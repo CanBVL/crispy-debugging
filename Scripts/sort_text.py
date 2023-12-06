61a65c5849c5dbfb76af44410fd1cf4772fc8cf6
Erlan Tolenbaev <alanwake46@gmail.com>
2023-12-06 18:31:05 +0600

    Added a description of the script, also added a function for closing files.

--- Scripts/sort_text.py
+++ Scripts/sort_text.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
-input_file = "Glosarry.txt"
-output_file = "Glosarry_sorted.txt"
+#This script receives a .text file, sorts it alphabetically, and creates a new file with the final result
+
+input_file = <File_name>
+output_file = <File_name>
 with open(input_file, 'r') as f:
     with open(output_file, 'w') as o:
         o.write("\n".join(sorted(f.read().splitlines())))
+f.close()
+o.close()
