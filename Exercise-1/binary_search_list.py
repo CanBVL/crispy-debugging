3eab5983586375285a1e2934a3e99362b1a711f5
Erlan Tolenbaev <alanwake46@gmail.com>
2023-12-08 18:28:27 +0600

    Add script that performs a binary search in given list

---
+++ Exercise-1/binary_search_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def binary_search(list, key):
+	list.sort() # Binary search starts with a sorted list
+	left = 0 # The first value of the list
+	right = len(list) - 1 # The last value of the list
+	while left <= right:
+		middle = (left + right) // 2
+		if list[middle] == key:
+			return middle
+		if list[middle] > key:
+			print("Checking the left side")
+			right = middle - 1
+		if list[middle] < key:
+			print("Checking the right side")
+			left = middle + 1
+	return -1
+
+print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
